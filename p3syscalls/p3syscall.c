1f141a60be74e437d72336ce51a9f448119e7ec7
JunHuang <mightybeesun@gmail.com>
2014-11-10 11:28:04 -0500

    Fixed the variable length problem.
    All functions tested, working as expected.

--- p3syscalls/p3syscall.c
+++ p3syscalls/p3syscall.c
@@ -17,8 +17,8 @@ char k_vardef[MAX_BUF_SIZE];
 asmlinkage long sys_SaveVariable(char __user *varname, char __user *vardef){
 	int i = 0 ;
 
-	int varnameLen = strlen(varname) + 1;
-	int vardefLen = strlen(vardef) +1;
+	int varnameLen = strlen(varname);
+	int vardefLen = strlen(vardef);
 	if ( (varnameLen < 1) || (varnameLen >  MAX_BUF_SIZE) ) {
         printk(KERN_EMERG "SaveVariable() varname failed: illegal len (%d)!", varnameLen);
         return(-1);
@@ -59,7 +59,7 @@ asmlinkage long sys_SaveVariable(char __user *varname, char __user *vardef){
 	return(-1);
 }
 
-asmlinkage long sys_GetVariable(char __user *varname, char __user *vardef, int deflen){
+asmlinkage long sys_GetVariable(char __user *varname, char __user *vardef, int __user *deflen){
 	int i;
 	int len;
 	int bResult;
@@ -94,13 +94,13 @@ asmlinkage long sys_GetVariable(char __user *varname, char __user *vardef, int d
 		        return(-1);
 		 	}
 		 	printk(KERN_EMERG "Copied var to user, the vardef is %s", vardef);
-		 	len = len+1;
+		 	
 		 	vardef[len] = '\0';
-		 	deflen = len;
-		 	/*if(copy_to_user(&deflen, &len, sizeof(int))) {
+		 	len = len+1;
+		 	if(copy_to_user(deflen, &len, sizeof(int))) {
 		 		printk(KERN_EMERG "Failed to copy to user for the variable length");
 		 		return(-1);
-		 	}*/
+		 	}
 		 	printk(KERN_EMERG "GetVariable() get vairable success!");
 		 	return(0);
 		}
@@ -111,7 +111,7 @@ asmlinkage long sys_GetVariable(char __user *varname, char __user *vardef, int d
 	return(-1);
 }
 
-asmlinkage long sys_NextVariable(char __user *prevname, char __user *varname, int namelen, char __user *vardef, int deflen){
+asmlinkage long sys_NextVariable(char __user *prevname, char __user *varname, int __user *namelen, char __user *vardef, int __user *deflen){
 
 	int i = 0;
 	int len;
@@ -121,26 +121,38 @@ asmlinkage long sys_NextVariable(char __user *prevname, char __user *varname, in
 				printk(KERN_EMERG "NextVariable() failed reached end of list");
 				return(-1);
 			}
+			i +=1;
 			
 			len = strlen(vardefs[i]);
 			if (copy_to_user(vardef, vardefs[i], len)) {
 		        printk(KERN_EMERG "NextVariable() failed: copy_to_user() error on vardef");
 		        return(-1);
 		 	}
-		 	len += 1;
+		 	
 		 	vardef[len] = '\0';
-		 	deflen = len;
-		 	printk(KERN_EMERG "NextVariable() get next variable success!");
+		 	len += 1;
 
+		 	if(copy_to_user(deflen, &len, sizeof(int))) {
+		 		printk(KERN_EMERG "Failed to copy to user for the variable length");
+		 		return(-1);
+		 	}
+
+		 	//printk(KERN_EMERG "NextVariable() get next variable success!");
+
+		 	len = 0;
 		 	len = strlen(varnames[i]);
 			if (copy_to_user(varname, varnames[i], len)) {
 		        printk(KERN_EMERG "NextVariable() failed: copy_to_user() error on varname");
 		        return(-1);
 		 	}
-		 	len += 1;
+		 	
 		 	varname[len] = '\0';
-		 	namelen = len;
+		 	len += 1;
 
+		 	if(copy_to_user(namelen, &len, sizeof(int))) {
+		 		printk(KERN_EMERG "Failed to copy to user for the variable length");
+		 		return(-1);
+		 	}
 		 	printk(KERN_EMERG "NextVariable() get next variable success!");
 		 	return(0);
 		}
