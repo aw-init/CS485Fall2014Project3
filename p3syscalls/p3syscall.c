106ce0d95ce9ea875ee69f0929a830c220711499
JunHuang <mightybeesun@gmail.com>
2014-11-10 01:23:51 -0500

    Tested save var and get var successful.
    Key things to remember when calling the sys function, when calling get var, in user program, the memory must be allocated, it is preferable to use things like 'char varname[1000];' to initialize variable, so throughout the code it is consistent.  This would guarantee the memory is allocated and writable to memory space.
    
    Feature added this commit:
    *In save var , check if var name already in the list, replace vardef for the varname already exists instead of adding new one.
    *Added P3_DEBUG definition so we can quickly change build for release build.
    
    Minor Changes:
    Modified length calculation in NextVariable function.

--- p3syscalls/p3syscall.c
+++ p3syscalls/p3syscall.c
@@ -3,17 +3,20 @@
 #include <asm/uaccess.h>
 
 
+#define P3_DEBUG 1
 #define MAX_BUF_SIZE 1000
 #define MAX_VAR_NUM 20
 
 char varnames[MAX_VAR_NUM][MAX_BUF_SIZE];
 char vardefs[MAX_VAR_NUM][MAX_BUF_SIZE];
 
+char emptyStr[MAX_BUF_SIZE];
+char k_varname[MAX_BUF_SIZE];
+char k_vardef[MAX_BUF_SIZE];
+
 asmlinkage long sys_SaveVariable(char __user *varname, char __user *vardef){
 	int i = 0 ;
-	char emptyStr[MAX_BUF_SIZE];
-	char k_varname[MAX_BUF_SIZE];
-	char k_vardef[MAX_BUF_SIZE];
+
 	int varnameLen = strlen(varname) + 1;
 	int vardefLen = strlen(vardef) +1;
 	if ( (varnameLen < 1) || (varnameLen >  MAX_BUF_SIZE) ) {
@@ -32,19 +35,20 @@ asmlinkage long sys_SaveVariable(char __user *varname, char __user *vardef){
         return(-1);
  	}
 
- 	if (copy_from_user(k_vardef, vardef, varnameLen)) {
+ 	if (copy_from_user(k_vardef, vardef, vardefLen)) {
         printk(KERN_EMERG "SaveVariable() failed: copy_from_user() error on vardef");
         return(-1);
  	}
 
 	for(; i < MAX_VAR_NUM; ++i){
-		if(!strcmp(varnames[i],emptyStr)){
+		if(!strcmp(varnames[i],emptyStr) || !strcmp(varnames[i],k_varname)){
 			strcpy(varnames[i],k_varname);
 			varnames[i][varnameLen] = '\0';
 
 			strcpy(vardefs[i],k_vardef);
 			vardefs[i][vardefLen] = '\0';
 
+			printk(KERN_EMERG "The variable name is %s, and the variable def is %s",varnames[i],vardefs[i]);
 			printk(KERN_EMERG "SaveVariable() save success!");
 			return(0);
 		}
@@ -56,23 +60,53 @@ asmlinkage long sys_SaveVariable(char __user *varname, char __user *vardef){
 }
 
 asmlinkage long sys_GetVariable(char __user *varname, char __user *vardef, int deflen){
-	
-	int i = 0;
+	int i;
+	int len;
+	int bResult;
+	int iCopyToUserRes;
+
+	printk(KERN_EMERG "Entered GetVar routine varname is %s", varname);
+
+	if (P3_DEBUG){
+		i = 0;
+		for(; i < MAX_VAR_NUM; i++){
+			printk(KERN_EMERG "at index %d the varname is %s and vardef is %s\n",i,varnames[i],vardefs[i]);
+
+			bResult = strcmp(varname,varnames[i]);
+
+			printk(KERN_EMERG "The result is %d",bResult);
+		}
+	}
+	i = 0;
 	for(; i < MAX_VAR_NUM ; i++){
-		if(!strcmp(varname,varnames[i])){
-			int len = strlen(vardefs[i])+1;
-			if (copy_to_user(vardef, vardefs[i], len)) {
+		if(strcmp(varname,varnames[i]) == 0){
+			printk(KERN_EMERG "Found matching var\n");
+			
+			
+			strcpy(k_vardef,vardefs[i]);
+			len = strlen(k_vardef);
+			printk(KERN_EMERG "The vardef length is %d , the value is %s\n",len,k_vardef);
+			iCopyToUserRes = copy_to_user(vardef, k_vardef, len);
+			printk(KERN_EMERG "The copy return value is %d\n" , iCopyToUserRes);
+			if(iCopyToUserRes) 
+			{
 		        printk(KERN_EMERG "GetVariable() failed: copy_to_user() error on vardef");
 		        return(-1);
 		 	}
+		 	printk(KERN_EMERG "Copied var to user, the vardef is %s", vardef);
+		 	len = len+1;
 		 	vardef[len] = '\0';
 		 	deflen = len;
+		 	/*if(copy_to_user(&deflen, &len, sizeof(int))) {
+		 		printk(KERN_EMERG "Failed to copy to user for the variable length");
+		 		return(-1);
+		 	}*/
 		 	printk(KERN_EMERG "GetVariable() get vairable success!");
 		 	return(0);
 		}
 	}
 
-	printk(KERN_EMERG "SaveVariable() failed, no variable named (%s) found!",varname);
+	printk(KERN_EMERG "GetVariable() failed, no variable named (%s) found!",varname);
 
 	return(-1);
 }
@@ -81,27 +115,29 @@ asmlinkage long sys_NextVariable(char __user *prevname, char __user *varname, in
 
 	int i = 0;
 	int len;
-	for(; i < MAX_VAR_NUM ; i++){
+	for(; i < MAX_VAR_NUM ; i++){	
 		if(!strcmp(prevname,varnames[i])){
 			if(i +1 >= MAX_VAR_NUM){
 				printk(KERN_EMERG "NextVariable() failed reached end of list");
 				return(-1);
 			}
 			
-			len = strlen(vardefs[i])+1;
+			len = strlen(vardefs[i]);
 			if (copy_to_user(vardef, vardefs[i], len)) {
 		        printk(KERN_EMERG "NextVariable() failed: copy_to_user() error on vardef");
 		        return(-1);
 		 	}
+		 	len += 1;
 		 	vardef[len] = '\0';
 		 	deflen = len;
 		 	printk(KERN_EMERG "NextVariable() get next variable success!");
 
-		 	len = strlen(varnames[i])+1;
+		 	len = strlen(varnames[i]);
 			if (copy_to_user(varname, varnames[i], len)) {
 		        printk(KERN_EMERG "NextVariable() failed: copy_to_user() error on varname");
 		        return(-1);
 		 	}
+		 	len += 1;
 		 	varname[len] = '\0';
 		 	namelen = len;
 
